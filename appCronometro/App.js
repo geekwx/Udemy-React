b62373fda799bad5b35eca9d15825cb8d85fadd1
GeekWx <geekwx@gmail.com>
2020-12-15 06:08:34 -0300

    Finalizando Cronometro

--- appCronometro/App.js
+++ appCronometro/App.js
@@ -7,8 +7,42 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
     super(props);
     this.state = {
       numero: 0, 
+      botao: 'VAI',
+      ultimo: null
     };
+    // Variavel do timer do relogio 
+    this.timer = null ;
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
 
+  }
+  vai(){
+
+    if(this.timer != null){
+      // Aqui vai parar o timer
+      clearInterval(this.timer);
+      this.timer = null;
+      this.setState({botao: 'VAI'});
+    }else {
+      //  Começa a girar o timer
+      this.timer = setInterval( () => {
+        this.setState({ numero: this.state.numero + 0.1})
+      }, 100);
+
+      this.setState({botao: 'PARAR'});
+    }
+
+  }
+  limpar(){
+    if(this.timer != null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    this.setState({
+      ultimo: this.state.numero,
+      numero: 0,
+      botao: 'VAI'
+    })
   }
 
    render(){
@@ -18,16 +52,21 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
       source={require('./src/cronometro.png')}
       style={styles.cronometro}
       />
-      <Text style={styles.timer}>{this.state.numero}</Text>
+      <Text style={styles.timer}>{this.state.numero.toFixed(1)}</Text>
       <View style={styles.btnArea}>
-      <TouchableOpacity style={styles.btn}>
-        <Text style={styles.btnTexto}>VAI</Text>
+      <TouchableOpacity style={styles.btn} onPress={this.vai}>
+  <Text style={styles.btnTexto}>{this.state.botao}</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={this.limpar}>
         <Text style={styles.btnTexto}>STOP</Text>
       </TouchableOpacity>
-      
+        
       </View>
+      <View style={styles.areaUltimo}>
+  <Text style={styles.textoCorrida}> 
+    {this.state.ultimo > 0 ? 'Ultimo tempo: ' + this.state.ultimo.toFixed(2) : ''}
+   </Text>
+        </View>
     </View>
   );
 }}
@@ -64,6 +103,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#00aeef',
 
+  },
+  areaUltimo: {
+    marginTop: 40
+  },
+  textoCorrida: {
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: "#FFF"
   }
 });
 export default App;
