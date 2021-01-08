7a421acb785f8ceed15ea36be5777b2e67574707
Suporte UBB <suporte@ubbonline.org.br>
2021-01-08 17:31:36 -0300

    continuacao..

--- app18/App.js
+++ app18/App.js
@@ -8,15 +8,28 @@ export default function App(){
   
   
     const [nome, setNome] = useState('Carregando...');
-  
+    const [idade, setIdade] = useState('')
     useEffect(() => {
+        async function dados(){
+          // listening (Olheiro) da nossa database - fica checando em loop no banco
+          await firebase.database().ref('usuarios/2').on('value', (snapshot) => {
+            setNome(snapshot.val().nome);
+            setIdade(snapshot.val().idade);
+          });
 
+          //Busca uma unica vez
+          // await firebase.database().ref('nome').once('value', (snapshot) => {
+          //   setNome(snapshot.val());
+          // })
+        }
+        dados();
     }, [])
   
     return(
 
     <View style={{marginTop: 25}}>
       <Text style={{fontSize: 25}}> Olá!!! {nome} </Text>
+      <Text style={{fontSize: 25}}> Idade {idade} </Text>
     </View> 
 
   );
